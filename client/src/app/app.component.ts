fec7c83c3cf38729ee9bb6ba099a553abfc63e93
DianaAsli <dianaasli201001@gmail.com>
2025-07-31 16:20:01 +0300

    Implement Header component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LayoutComponent } from "./core/layout/layout.component";
+import { LayoutComponent } from "./layout/layout/layout.component";
 
 @Component({
   selector: 'app-root',
---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isLoggedIn: boolean = true;
+  isDropDownOpen: boolean = true;
+
+  toggleDropDown() {
+    this.isDropDownOpen = !this.isDropDownOpen
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+  }
+}
