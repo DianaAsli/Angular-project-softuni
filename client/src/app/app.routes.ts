564de3e2d412fd736c123b2dac42be40799fed24
DianaAsli <dianaasli201001@gmail.com>
2025-07-31 23:57:19 +0300

    Add basic routing setup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { CategoriesComponent } from './features/categories/categories.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/home' },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'categories', component: CategoriesComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactsComponent }
 ];
