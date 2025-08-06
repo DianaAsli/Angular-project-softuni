03c404491b7821071497850bdc8932a340f7803c
DianaAsli <dianaasli201001@gmail.com>
2025-08-06 22:12:25 +0300

    Implement Login component

--- client/src/app/features/auth/login/login.component.ts
+++ client/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TitleComponent } from "../../../shared/components/title/title.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
 
+  form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+
+  login() {
+    const loginData = this.form.value;
+    console.log('login data', loginData);
+
+  }
 }
