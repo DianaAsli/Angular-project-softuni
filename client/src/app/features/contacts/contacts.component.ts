10178864cfb37c22d94964218c1b346f346c3170
DianaAsli <dianaasli201001@gmail.com>
2025-08-20 20:51:51 +0300

    Implement Contact component

--- client/src/app/features/contacts/contacts.component.ts
+++ client/src/app/features/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare const google: any;
 
 @Component({
   selector: 'app-contacts',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
-export class ContactsComponent {
+export class ContactsComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    // проверка дали google.maps е готов
+    if (typeof google !== 'undefined' && google.maps) {
+      this.initMap();
+    } else {
+      // ако скриптът още не е зареден, изчакай малко
+      const interval = setInterval(() => {
+        if (typeof google !== 'undefined' && google.maps) {
+          clearInterval(interval);
+          this.initMap();
+        }
+      }, 100);
+    }
+  }
+
+  private initMap(): void {
+    const marker = { lat: 42.6936, lng: 23.3190 };
+    
+    const map = new google.maps.Map(
+      document.getElementById('map') as HTMLElement,
+      {
+        center: marker,
+        zoom: 16
+      }
+    );
+
+    new google.maps.Marker({
+      position: marker,
+      map: map
+    });
+  }
 }
